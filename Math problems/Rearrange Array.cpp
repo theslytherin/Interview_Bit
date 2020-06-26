5b71932ade30ef4447c3aa113f8a19dd11eec43d
theslytherin <anuskasinha1200@gmail.com>
2020-06-26 23:34:08 +0530

    Math

---
+++ Math problems/Rearrange Array.cpp
@@ -0,0 +1,13 @@
+class Solution:
+    # @param A : list of integers
+    # Modify the array A which is passed by reference. 
+    # You do not need to return anything in this case. 
+    def arrange(self, A):
+        n=len(A)
+        for i in range(0, n):
+            A[i] =A[i]+( (A[A[i]] % n) * n )
+  
+    
+        for i in range(0, n):
+            A[i] = int(A[i] / n)
+        return A
