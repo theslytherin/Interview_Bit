5e25f43e811f64d35b7dbd51c04473047f49bb76
theslytherin <anuskasinha1200@gmail.com>
2020-06-26 23:29:00 +0530

    Math

---
+++ Math problems/Power Of Two Integers.cpp
@@ -0,0 +1,16 @@
+int Solution::isPower(int A) {
+    if (A<2)
+        return true;
+  
+    for (auto i = 2; i<=sqrt(A); ++i)
+    {
+        for (auto j = 2; j<=32; ++j)
+        {
+            if(pow(i, j)==A)
+                return true;
+        }
+    }
+    return false;
+}
+
+
