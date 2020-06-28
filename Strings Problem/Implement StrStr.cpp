18155bc38b3c683d3b069bfd51c26ccc6900dea8
theslytherin <anuskasinha1200@gmail.com>
2020-06-28 22:57:50 +0530

    Strings Problem

---
+++ Strings Problem/Implement StrStr.cpp
@@ -0,0 +1,30 @@
+int Solution::strStr(const string A, const string B) {
+int index = -1;
+    if (A.empty() || B.empty())
+        return index;
+    for (auto i = 0; i<A.length(); ++i)
+    {
+        if (A[i]==B[0])
+        {
+            index = i;
+            int j = 0;
+            while (j<B.length())
+            {
+                if (B[j]==A[i] && i<A.length())
+                {
+                    ++i; ++j;
+                }
+                else
+                {
+                    index = -1;
+                    i -= j;
+                    break;
+                }
+            }
+            if (j==B.length())
+                return index;
+        }
+    }
+    return index;
+}
+
